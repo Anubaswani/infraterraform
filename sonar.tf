439e91c6ce5c2fe1bb0dc03c2b8b511d4ddad558
Anubaswani <118058834+Anubaswani@users.noreply.github.com>
2024-02-15 10:49:25 +0530

    Update sonar.tf

--- sonar.tf
+++ sonar.tf
@@ -1,16 +1,16 @@
-resource "aws_instance" "mySonarInstance" {
-      ami           = "ami-0e6329e222e662a52"
+resource "aws_instance" "Project" {
+      ami           = "var.ami"
       key_name = var.key_name
       instance_type = "t2.micro"
-      vpc_security_group_ids = [aws_security_group.sonar-sg-2022.id]
+      vpc_security_group_ids = [sg-0fb6eea9297c32b3e]
       tags= {
-        Name = "sonar_instance"
+        Name = "Project_instance"
       }
     }
 
- resource "aws_security_group" "sonar-sg-2022" {
-      name        = "security_sonar_group_2022"
-      description = "security group for Sonar"
+ resource "aws_security_group" "Project-sg-2022" {
+      name        = "security_Project_group_2022"
+      description = "security group for Project"
 
       ingress {
         from_port   = 9000
@@ -26,7 +26,7 @@ resource "aws_instance" "mySonarInstance" {
         cidr_blocks = ["0.0.0.0/0"]
       }
 
-     # outbound from Sonar server
+     # outbound from Project server
       egress {
         from_port   = 0
         to_port     = 65535
@@ -35,15 +35,15 @@ resource "aws_instance" "mySonarInstance" {
       }
 
       tags= {
-        Name = "security_sonar"
+        Name = "security_Project"
       }
     }
 
-# Create Elastic IP address for Sonar instance
-resource "aws_eip" "mySonarInstance" {
+# Create Elastic IP address for Project instance
+resource "aws_eip" "myProjectInstance" {
   vpc      = true
-  instance = aws_instance.mySonarInstance.id
+  instance = aws_instance.myProjectInstance.id
 tags= {
-    Name = "sonar_elastic_ip"
+    Name = "Project_elastic_ip"
   }
 }
