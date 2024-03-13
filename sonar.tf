a6e564d49c1b7c17c599435f3164fb9e84aa0146
Anubaswani <118058834+Anubaswani@users.noreply.github.com>
2024-03-13 12:27:09 +0530

    Update sonar.tf

--- sonar.tf
+++ sonar.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "mySonarInstance" {
       ami           = "ami-022661f8a4a1b91cf"
       key_name = var.key_name
       instance_type = "t2.micro"
-      vpc_security_group_ids = ["aws_security_group.sonar-sg-2022.name"]
+      vpc_security_group_ids = [aws_security_group.sonar-sg-2022.name]
       tags= {
         Name = "Sonar_instance"
       }
